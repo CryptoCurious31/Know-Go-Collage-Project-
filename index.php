5c6efa6c7fcdd9d1ce9ca4f4a0659f33c5890404
Saad <info.cryptocurious@gmail.com>
2024-05-12 23:55:01 +0500

    Navigation bar

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Know Go | The light for the blind people</title>
+    <!-- ===== Css link ====== -->
+    <link rel="stylesheet" href="Css/style.css">
+    <!-- ===== BootStrap Css link ===== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <!-- ===== Font Awesome link ===== -->
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <nav class="navigation-bar">
+            <div class="logo">Know Go</div>
+            <div class="links">
+                <ul>
+                    <a href="index.php"><li>Home</li></a>
+                    <a href="pricing.php"><li>Packages</li></a>
+                    <a href="about.php"><li>About</li></a>
+                    <a href="feedback.php"><li>Feedback</li></a>
+                </ul>
+            </div>
+            <div class="register">
+                <button type="submit" class="sign-in">Login</button>
+                <button type="submit" class="sign-up">Sign Up</button>
+            </div>
+        </nav>
+    </header>    
+</body>
+</html> 
