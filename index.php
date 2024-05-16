261e89dab2e4bc60dc66fda1c99ad8370769d824
Saad <info.cryptocurious@gmail.com>
2024-05-16 20:05:05 +0500

    Header and Hero Section Completed

--- index.php
+++ index.php
@@ -1,40 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Know Go | The light for the blind people</title>
-    <!-- ===== Css link ====== -->
-    <link rel="stylesheet" href="Css/style.css">
-    <!-- ===== BootStrap Css link ===== -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-    crossorigin="anonymous">
-    <!-- ===== Font Awesome link ===== -->
-    <link rel="stylesheet" 
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
-    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
-    crossorigin="anonymous" 
-    referrerpolicy="no-referrer" />
+    <title>Know Go | An Eye for the Blind People</title>
+    <!-- ======== Bootstrap Css file link ======= -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- ========= External Directory Css File link ======== -->
+    <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    <header>
-        <nav class="navigation-bar">
-            <div class="logo">Know Go</div>
-            <div class="links">
-                <ul>
-                    <a href="index.php"><li>Home</li></a>
-                    <a href="pricing.php"><li>Packages</li></a>
-                    <a href="about.php"><li>About</li></a>
-                    <a href="feedback.php"><li>Feedback</li></a>
-                </ul>
+    <main>
+        <!-- ==== Hero Section ==== -->
+        <section class="hero">
+            <!-- ==== Header Section ==== -->
+            <header class="container-fluid">
+                <nav>
+                    <div class="logo">
+                        <h2>Know Go</h2>
+                    </div>
+                    <ul>
+                        <a href="#">
+                            <li>Home</li>
+                        </a>
+                        <a href="#">
+                            <li>Package</li>
+                        </a>
+                        <a href="#">
+                            <li>Info</li>
+                        </a>
+                        <a href="#">
+                            <li>Feedback</li>
+                        </a>
+                        <a href="#">
+                            <li>Contact Us</li>
+                        </a>
+                    </ul>
+                </nav>
+            </header>
+            <!-- ==== Hero Section Content ==== -->
+            <div class="container-fluid">
+                <div class="column">
+                <p>An Eye for the blind people</p>
+                <h4>Know Go</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda praesentium animi!
+                    Voluptates facilis ad itaque fuga libero magnam soluta eum, dignissimos ab fugit facere accusamus
+                    quo officia consequatur asperiores, sed hic rerum porro?</p>
+                <div class="button">
+                    <button type="submit">Buy now</button>
+                </div>
+                </div>
             </div>
-            <div class="register">
-                <button type="submit" class="sign-in">Login</button>
-                <button type="submit" class="sign-up">Sign Up</button>
-            </div>
-        </nav>
-    </header>    
+        </section>
+    </main>
 </body>
-</html> 
+
+</html>
